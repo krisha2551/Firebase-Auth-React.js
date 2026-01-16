82e71bf86fe624c9cccc7555371b505bf9479526
krisha2551 <krishajasani62@gmail.com>
2026-01-16 15:07:11 +0530

    Fix formatting issues in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Auth from "./pages/Auth";
-import StudentForm from "./pages/StudentForm";
+
 
 export default function App() {
   return (
@@ -9,3 +9,4 @@ export default function App() {
     </>
   );
 }
+
